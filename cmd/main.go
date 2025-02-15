5fa31696ef139f98bc0b481aecc3067d89a63e17
Stanislav Shamilov <shamilovstas@protonmail.com>
2025-02-15 21:40:54 +0200

    adds aws lambda using api gateway

--- cmd/main.go
+++ cmd/main.go
@@ -7,86 +7,26 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-/*import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"log/slog"
-	"os"
-	"strings"
-
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-)
-
-var (
-	s3Client *s3.Client
-)
-
-type Order struct {
-	OrderId string  `json:"order_id"`
-	Amount  float64 `json:"amount"`
-	Item    string  `json:"item"`
-}
-
-func init() {
-	cfg, err := config.LoadDefaultConfig(context.Background())
-	if err != nil {
-		slog.Error("init aws config", slog.Any("error", err))
-		os.Exit(1)
-	}
-
-	s3Client = s3.NewFromConfig(cfg)
-}
-
-func handler(ctx context.Context, event json.RawMessage) error {
-	var order Order
-	if err := json.Unmarshal(event, &order); err != nil {
-		slog.Error("unmarshal json", slog.Any("error", err))
-		return err
-	}
-
-	bucketName := os.Getenv("RECEIPT_BUCKET")
-	if bucketName == "" {
-		slog.Error("RECEIPT_BUCKET env variable is not set")
-		return fmt.Errorf("missing required environment variable: RECEIPT_BUCKET")
-	}
-
-	receiptContent := fmt.Sprintf("Order ID: %s\nAmount: $%.2f\nItem: %s", order.OrderId, order.Amount, order.Item)
-	key := "receipts/" + order.OrderId + ".txt"
-
-	if err := uploadReceipt(ctx, bucketName, key, receiptContent); err != nil {
-		return err
-	}
-	slog.Info("Successfully processed order and stored receipt in bucket", slog.String("order", order.OrderId), slog.String("bucket", bucketName))
-	return nil
-}
-
-func uploadReceipt(ctx context.Context, bucketName, key, receiptContent string) error {
-	_, err := s3Client.PutObject(ctx, &s3.PutObjectInput{
-		Bucket: &bucketName,
-		Key:    &key,
-		Body:   strings.NewReader(receiptContent),
-	})
-
-	if err != nil {
-		slog.Error("Failed to upload receipt", slog.Any("error", err))
-		return err
-	}
-	return nil
-}
-
-*/
-
 type Message struct {
 	Msg string `json:"message"`
 }
 
-func handler(ctx context.Context) (string, error) {
+type Response struct {
+	StatusCode int               `json:"statusCode"`
+	Headers    map[string]string `json:"headers"`
+	Body       json.RawMessage   `json:"body"`
+}
+
+func handler(ctx context.Context) (Response, error) {
 	message := Message{Msg: "Hello,1211212asdasdasd world!!!"}
 	raw, err := json.Marshal(message)
-	return string(raw), err
+
+	response := Response{
+		StatusCode: 200,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		Body:       raw,
+	}
+	return response, err
 }
 
 func main() {
--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 locals {
-    filename = "${path.module}/payload.zip"
+  filename = "${path.module}/payload.zip"
 }
 
 data "archive_file" "lambda-payload" {
-  type = "zip"
+  type        = "zip"
   source_file = "${path.module}/bootstrap"
   output_path = "${path.module}/payload.zip"
 }
@@ -17,7 +17,7 @@ data "aws_iam_policy_document" "assume_role" {
     effect = "Allow"
 
     principals {
-      type = "Service"
+      type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
 
@@ -26,35 +26,54 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "lambda_iam" {
-    name = "lambda-iam"
-    assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  name               = "lambda-iam"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_apigatewayv2_api" "simple-lambda" {
+  name          = "simple-lambda-gw"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "simple-lambda" {
+  api_id      = aws_apigatewayv2_api.simple-lambda.id
+  name        = "dev"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "simple-lambda-integration" {
+  api_id             = aws_apigatewayv2_api.simple-lambda.id
+  integration_uri    = aws_lambda_function.simple-lambda.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+  connection_type    = "INTERNET"
+}
+
+resource "aws_apigatewayv2_route" "simple-lambda" {
+  api_id    = aws_apigatewayv2_api.simple-lambda.id
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.simple-lambda-integration.id}"
+}
+
+resource "aws_lambda_permission" "api-gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.simple-lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.simple-lambda.execution_arn}/*/*"
 }
 
 resource "aws_lambda_function" "simple-lambda" {
-    function_name = "simple-lambda-with-go"
-    filename = data.archive_file.lambda-payload.output_path
-    role = aws_iam_role.lambda_iam.arn
-    handler = "bootstrap"
+  function_name = "simple-lambda-with-go"
+  filename      = data.archive_file.lambda-payload.output_path
+  role          = aws_iam_role.lambda_iam.arn
+  handler       = "bootstrap"
 
-    source_code_hash = data.archive_file.lambda-payload.output_base64sha256
-    runtime = "provided.al2023"
+  source_code_hash = data.archive_file.lambda-payload.output_base64sha256
+  runtime          = "provided.al2023"
 }
 
-resource "aws_lambda_function_url" "simple-lambda" {
-    function_name = aws_lambda_function.simple-lambda.function_name
-    authorization_type = "NONE"
-}
+output "base_url" {
+  value = aws_apigatewayv2_stage.simple-lambda.invoke_url
 
-
-output "lambda_iam" {
-    description = "Lambda IAM"
-    value = data.aws_iam_policy_document.assume_role.json
-}
-
-output "lambda_url" {
-    value = aws_lambda_function_url.simple-lambda.function_url
-}
-
-output "payload_hash" {
-    value = data.archive_file.lambda-payload.output_base64sha256
 }
